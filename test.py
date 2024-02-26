99d5da316ba578d21cac3bd6c7699d348d6d1355
Cristian Gorup <116388024+Balgem@users.noreply.github.com>
2024-02-26 01:06:00 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,6 @@
 from main import model_pred
 
-new_data = {'Present_Price': 1.309818,
-            'Fuel_Type': 2,
-            'Seller_Type': 2,
-            'Transmission': 2,
-            'Owner': 0,
-            'logAge': 0.079181,
-            'logKMSDriven': 0.016381,
-            'PrecenPriceyLogAge': 0.103713,
-            'PrecentPriceyFuel': 2.619636,
+new_data = {'TV': 100.3,
             }
 
 
